b20a03add6a382319a12e4e8b329f7ef3f54dc4d
Mjulian150708 <144661495+Mjulian150708@users.noreply.github.com>
2023-09-21 10:39:28 +0700

    Create Operasi penjumlahan.cpp

---
+++ Operasi penjumlahan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"program penjumlahan \n";
+    cout<<"Masukkan Angka Pertama:";
+    cin>>a;
+    cout<<"masukkan Angka Kedua:";
+    cin>>b;
+    c=a+b;
+    cout<<"Hasilnya adalah:"<<c<<endl;
+    return 0;
+}
